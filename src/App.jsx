ab986a01ed0b939b042fac504ad164f640f77b2b
tbs01215 <tbs01215@egmail.com>
2023-06-18 22:13:22 +0900

    Feature: Add QueryProvider for React Query and Move global styles from app.jsx to index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,10 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 import bgImg from "./assets/background-img/qhd/tony-lee-i_XLLP08BOc-unsplash.jpg";
 import Bottom from "./components/layout/Bottom";
 import Center from "./components/layout/Center";
 import Top from "./components/layout/Top";
 
-const GlobalStyle = createGlobalStyle`
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  
-  font-size: 1.1em;
-}
-html {
-  font-size: 16px;
-}
-li{
-  list-style: none;
-}
-body {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
-
-input,
-input:focus {
-  outline: none;
-  border: none;
-  background-color: inherit;
-}
-
-input {
-  border-bottom: grey 1.3px solid;
-  transition: 0.3s;
-  color: inherit;
-}
-
-input:focus {
-  border-bottom: greenyellow 1.3px solid;
-}
-
-input::placeholder {
-  color: rgba(255, 255, 255, 0.8);
-}
-
-.centered {
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-`;
-
-const AppContainer = styled.div`
+const Container = styled.div`
   padding: 8px;
   height: 100vh;
   color: white;
@@ -77,12 +25,11 @@ const AppContainer = styled.div`
 function App() {
   return (
     <>
-      <GlobalStyle></GlobalStyle>
-      <AppContainer className="App">
+      <Container className="App">
         <Top></Top>
         <Center></Center>
         <Bottom></Bottom>
-      </AppContainer>
+      </Container>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,70 @@ import "./index.css";
 import App from "./App";
 import { RecoilRoot } from "recoil";
 import { Normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  
+  font-size: 1.1em;
+}
+html {
+  font-size: 16px;
+}
+li{
+  list-style: none;
+}
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+input,
+input:focus {
+  outline: none;
+  border: none;
+  background-color: inherit;
+}
+
+input {
+  border-bottom: grey 1.3px solid;
+  transition: 0.3s;
+  color: inherit;
+}
+
+input:focus {
+  border-bottom: greenyellow 1.3px solid;
+}
+
+input::placeholder {
+  color: rgba(255, 255, 255, 0.8);
+}
+
+.centered {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+`;
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
-    <Normalize />
-    <App />
+    <QueryClientProvider client={client}>
+      <Normalize />
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </RecoilRoot>
 );
