1e2d3ef1b317a617337a7f079382de273b348501
tbs01215 <tbs01215@egmail.com>
2023-06-18 23:04:16 +0900

    refactor: Replace useEffect with usePosition and React Query for weather data fetching

--- src/components/Weather/Index.jsx
+++ src/components/Weather/Index.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { getWeatherData } from "../../APIs/weatherMap";
 import { motion } from "framer-motion";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import { usePosition } from "../hooks/usePosition";
 
 const WeatherContainer = styled.section``;
 const Div = styled(motion.div)`
@@ -11,48 +13,34 @@ const Img = styled(motion.img)`
   height: 80px;
 `;
 export default function Weather() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [weatherData, setWeatherData] = useState(null);
-
-  useEffect(() => {
-    if (navigator?.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async (pos) => {
-          const { latitude: lat, longitude: lon } = pos.coords;
-
-          const weatherData = await getWeatherData([lat, lon]);
-          // console.log(weatherData);
-          setIsLoading(false);
-          const currentWeather = weatherData.weather[0].main;
-          const currentTemp = weatherData.main.temp - 273.15;
-          const weatherIcon = weatherData.weather[0].icon;
-          setWeatherData({ currentWeather, currentTemp, weatherIcon });
-        },
-        (err) => {
-          alert(
-            "크롬 브라우저가 아니거나 위치 추적을 허용하지 않은 경우 날씨가 표시되지 않을 수 있습니다."
-          );
-          console.error(err);
-        }
-      );
-    }
-  }, []);
+  const { latitude, longitude } = usePosition();
+  const { isLoading, data, error } = useQuery(
+    ["weather"],
+    getWeatherData.bind(null, latitude, longitude)
+  );
+  console.log(data);
+  if (error) {
+    alert(
+      "크롬 브라우저가 아니거나 위치 추적을 허용하지 않은 경우 날씨가 표시되지 않을 수 있습니다."
+    );
+    console.error(error);
+  }
 
   return (
     <WeatherContainer>
       {isLoading ? (
-        "~ Loading ~"
+        "Loading..."
       ) : (
         <Div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { duration: 2 } }}
         >
           <div>
-            <div>{weatherData.currentWeather}</div>
-            <div>{Math.round(weatherData.currentTemp)}°C</div>
+            <div>{data.weather[0].main}</div>
+            <div>{Math.round(data.main.temp - 273.15)}°C</div>
           </div>
           <Img
-            src={`http://openweathermap.org/img/wn/${weatherData.weatherIcon}@2x.png`}
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
           ></Img>
         </Div>
       )}
